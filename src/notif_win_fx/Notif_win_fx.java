b001cf63f66ed093755a49e9c963dcd1663b7a87
OussemaMahjoub <47174218+OussemaMahjoub@users.noreply.github.com>
2019-04-20 03:14:03 +0100

    first

---
+++ src/notif_win_fx/Notif_win_fx.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package notif_win_fx;
+
+/**
+ *
+ * @author mahjoub
+ */
+public class Notif_win_fx {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+   
+       
+     new Notif("hahahah", "hihihihihi", "windows");
+    }
+    
+}
